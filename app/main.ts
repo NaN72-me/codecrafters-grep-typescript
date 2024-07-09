f66fb80ad093c3be70424d26b77f9723484acb0d
nhanlc <lecongnhan29@gmail.com>
2024-07-09 09:38:13 +0700

    feat(core): impl \d

--- app/main.ts
+++ app/main.ts
@@ -1,27 +1,34 @@
+import * as sugar from "sugar";
+import _ from "lodash";
+
 const args = process.argv;
 const pattern = args[3];
-
-const inputLine: string = await Bun.stdin.text();
-
-function matchPattern(inputLine: string, pattern: string): boolean {
-  if (pattern.length === 1) {
-    return inputLine.includes(pattern);
-  } else {
-    throw new Error(`Unhandled pattern: ${pattern}`);
-  }
-}
 
 if (args[2] !== "-E") {
   console.log("Expected first argument to be '-E'");
   process.exit(1);
 }
 
-// You can use print statements as follows for debugging, they'll be visible when running tests.
-console.log("Logs from your program will appear here!");
+function matchPattern(inputLine: string, pattern: string): boolean {
+  if (pattern.length === 1) {
+    return inputLine.includes(pattern);
+  }
 
-// Uncomment this block to pass the first stage
-if (matchPattern(inputLine, pattern)) {
-  process.exit(0);
-} else {
-  process.exit(1);
+  if (_.includes(pattern, "\\d"))
+    return sugar.Array.some(inputLine.split(""), c=> !_.isNaN(Number.parseInt(c, 10)));
+
+  throw new Error(`Unhandled pattern: ${pattern}`);
 }
+
+async function main(){
+  const inputLine: string = await Bun.stdin.text();
+
+  if (!matchPattern(inputLine, pattern)) {
+    process.exit(1);
+  }
+
+  process.exit(0);
+}
+
+main()
+    .then(r => console.log(r));
