12db4559af2160ec5d44b5a9a8a9fd5bc562f11f
nhanlc <lecongnhan29@gmail.com>
2024-07-09 09:50:45 +0700

    feat(core): impl /w

--- app/main.ts
+++ app/main.ts
@@ -9,13 +9,29 @@ if (args[2] !== "-E") {
   process.exit(1);
 }
 
+const CHAR_CODE_A = 65;
+const CHAR_CODE_Z = 90;
+const CHAR_CODE_a = 97;
+const CHAR_CODE_z = 122;
+
+function isDigit(c: string): boolean {
+  return !_.isNaN(Number.parseInt(c, 10));
+}
+
 function matchPattern(inputLine: string, pattern: string): boolean {
   if (pattern.length === 1) {
     return inputLine.includes(pattern);
   }
 
   if (_.includes(pattern, "\\d"))
-    return sugar.Array.some(inputLine.split(""), c=> !_.isNaN(Number.parseInt(c, 10)));
+    return sugar.Array.some(inputLine.split(""), c=> isDigit(c));
+
+  if (_.includes(pattern, "\\w"))
+    return sugar.Array.some(inputLine.split(""), c=>
+        c.charCodeAt(0) >= CHAR_CODE_A && c.charCodeAt(0) <= CHAR_CODE_Z ||
+        c.charCodeAt(0) >= CHAR_CODE_a && c.charCodeAt(0) <= CHAR_CODE_z ||
+        isDigit(c)
+    );
 
   throw new Error(`Unhandled pattern: ${pattern}`);
 }
@@ -24,6 +40,7 @@ async function main(){
   const inputLine: string = await Bun.stdin.text();
 
   if (!matchPattern(inputLine, pattern)) {
+    console.log("You have failed me!", inputLine, pattern);
     process.exit(1);
   }
 
---
+++ tests.sh
@@ -0,0 +1,2 @@
+echo "123" | ./your_grep.sh -E "\d"
+echo "alpha-num3ric" | ./your_grep.sh -E "\w"
